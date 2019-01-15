d6d790ab74a613495f32535993fb5b6529de6bff
Roberto Matheus <rsmatheusjr@hotmail.com>
2019-01-15 18:43:13 -0200

    Execoes no tabuleiro e programacao defensiva

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,10 +7,12 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
+	
 	public ChessMatch() {
 		board = new Board(8,8);
 		initialSetup();
 	}
+	
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for(int i=0;i<board.getRows();i++) {
@@ -20,7 +22,6 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	
 	private void initialSetup() {
 		board.placePiece(new Rook(board,Color.WHITE), new Position(2,1));
 		board.placePiece(new King(board,Color.BLACK), new Position(0,4));
