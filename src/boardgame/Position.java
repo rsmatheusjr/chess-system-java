cebaced63191a176bac87d211d87090e62533db5
Roberto Matheus <rsmatheusjr@hotmail.com>
2019-01-15 17:24:11 -0200

    Comecando a implementar o tabuleiro e pecas

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,13 +1,14 @@
 package boardgame;
 
 public class Position {
-
 	private int row;
 	private int column;
+	
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
+	
 	public int getRow() {
 		return row;
 	}
@@ -24,5 +25,4 @@ public class Position {
 	public String toString() {
 		return row+", "+column;
 	}
-	
 }
