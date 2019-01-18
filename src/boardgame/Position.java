bd0e099b5e5364bd3e401a791993a35b22346caa
Roberto Matheus <rsmatheusjr@hotmail.com>
2019-01-18 03:17:25 -0200

    Movendo Pecas

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,26 +3,30 @@ package boardgame;
 public class Position {
 	private int row;
 	private int column;
-	
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
-	
+
 	public int getRow() {
 		return row;
 	}
+
 	public void setRow(int row) {
 		this.row = row;
 	}
+
 	public int getColumn() {
 		return column;
 	}
+
 	public void setColumn(int column) {
 		this.column = column;
 	}
+
 	@Override
 	public String toString() {
-		return row+", "+column;
+		return row + ", " + column;
 	}
 }
